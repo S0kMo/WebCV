78b5cfe08661b9db30ce91389779b60f977983d8
Sum Somony <141939665+S0kMo@users.noreply.github.com>
2026-01-27 16:01:51 +0700

    Remove Flutter error handling in main function
    Removed error handling for Flutter in release mode.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
 // nh mij dg nh tver tam youtube dea ng 😐
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:webcv/constants/app_theme.dart';
 import 'package:webcv/locator.dart';
 import 'package:webcv/views/layout_template/layout_template.dart';
 
 void main() {
-  FlutterError.onError = (details) {
-    FlutterError.presentError(details);
-    if (kReleaseMode) exit(1);
-  };
   setupLocator();
   runApp(MyApp());
 }
